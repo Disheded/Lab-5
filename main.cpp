2041914ff93383f5cf31185857af106b95fc8f54
Disheded <hgfgvvg.ru@gmail.com>
2025-04-29 10:30:15 +0300

    Individual task complited

--- main.cpp
+++ main.cpp
@@ -9,6 +9,105 @@
 #include <queue>
 using namespace std;
 
+class Account {
+public:
+    string owner;
+    double balance;
+
+    Account() {}
+    Account(string o, double b) {
+        owner = o;
+        balance = b;
+    }
+};
+
+class Action {
+public:
+    string type; // "add", "delete", "edit"
+    int number;
+    Account oldData;
+
+    Action(string t, int n, Account oldAcc) {
+        type = t;
+        number = n;
+        oldData = oldAcc;
+    }
+};
+
+class BankSystem {
+private:
+    map<int, Account> accounts;
+    stack<Action> actions;
+
+public:
+    void add(int number, string name, double balance) {
+        accounts[number] = Account(name, balance);
+        actions.push(Action("add", number, Account()));
+        cout << "Account added.\n";
+    }
+
+    void remove(int number) {
+        if (accounts.count(number)) {
+            actions.push(Action("delete", number, accounts[number]));
+            accounts.erase(number);
+            cout << "Account deleted.\n";
+        } else {
+            cout << "Account isnt found.\n";
+        }
+    }
+
+    void edit(int number, string newName, double newBalance) {
+        if (accounts.count(number)) {
+            actions.push(Action("edit", number, accounts[number]));
+            accounts[number] = Account(newName, newBalance);
+            cout << "Account changed.\n";
+        } else {
+            cout << "Account isnt found.\n";
+        }
+    }
+
+    void showAll() {
+        if (accounts.empty()) {
+            cout << "No accounts.\n";
+            return;
+        }
+        for (auto& a : accounts) {
+            cout << "Account: " << a.first << ", Owner: " << a.second.owner << ", Balance: " << a.second.balance << endl;
+        }
+    }
+
+    void total() {
+        double sum = 0;
+        for (auto& a : accounts) {
+            sum += a.second.balance;
+        }
+        cout << "Total balance: " << sum << endl;
+    }
+
+    void undo() {
+        if (actions.empty()) {
+            cout << "No operations to undo.\n";
+            return;
+        }
+
+        Action act = actions.top();
+        actions.pop();
+
+        if (act.type == "add") {
+            accounts.erase(act.number);
+            cout << "Undo add.\n";
+        } else if (act.type == "delete") {
+            accounts[act.number] = act.oldData;
+            cout << "Undo delete.\n";
+        } else if (act.type == "edit") {
+            accounts[act.number] = act.oldData;
+            cout << "Undo edit.\n";
+        }
+    }
+};
+
+
+
 int main() {
     while (true) {
         int number;
@@ -184,9 +283,49 @@ int main() {
 
         }
         if (number == 5) {
+            BankSystem bank;
+            int choice;
 
+            while (true) {
+                cout << "\n1. Add\n2. Remove\n3. Edit\n4. Show all\n5. Total balance\n6. Undo\n0. Exit\nYour choice: ";
+                cin >> choice;
 
+                int number;
+                string name;
+                double balance;
 
+                if (choice == 0) break;
+
+                switch (choice) {
+                    case 1:
+                        cout << "Account number: "; cin >> number;
+                        cout << "Owner name: "; cin >> ws; getline(cin, name);
+                        cout << "Initial balance: "; cin >> balance;
+                        bank.add(number, name, balance);
+                        break;
+                    case 2:
+                        cout << "Account number: "; cin >> number;
+                        bank.remove(number);
+                        break;
+                    case 3:
+                        cout << "Account number: "; cin >> number;
+                        cout << "New owner name: "; cin >> ws; getline(cin, name);
+                        cout << "New balance: "; cin >> balance;
+                        bank.edit(number, name, balance);
+                        break;
+                    case 4:
+                        bank.showAll();
+                        break;
+                    case 5:
+                        bank.total();
+                        break;
+                    case 6:
+                        bank.undo();
+                        break;
+                    default:
+                        cout << "Invalid choice.\n";
+                }
+            }
 
 
 
